6abad9168c7e68c09aadeec000e304c5d1624c8b
vaithschmitz <vaithschmitz@googlemail.com>
2019-09-26 22:07:41 +0100

    client-side routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,16 +6,16 @@ export default function Navbar(){
     return(
         <div id='Navbar-container'>
             <div id='Navbar-middle'>
-                <a href='https://vaith.dev/about'><div id='Navbar-Who'>ABOUT</div> </a>
+                <a href='/about'><div id='Navbar-Who'>ABOUT</div> </a>
                 <div id='Navbar-Work' onClick={()=> setExpanded(!expand)}>WORK
                     <div id={expand ? 'Navbar-Work-dropdown' : 'Navbar-Work-hidden'} >
-                        <a href='https://vaith.dev/mswalk'><div id='Navbar-Work-MS'>MS Walk</div></a>
+                        <a href='/mswalk'><div id='Navbar-Work-MS'>MS Walk</div></a>
                         <div id='Navbar-Work-memefy'>Memefy</div>
                         <div id='Navbar-Work-Synth'>WTSynth</div>
                         <div id='Navbar-Work-Jarvis'>Home Automation</div>
                     </div>
                 </div>
-                <a href='https://vaith.dev/contact'><div id='Navbar-Talk'>LET'S TALK</div></a>
+                <a href='/contact'><div id='Navbar-Talk'>LET'S TALK</div></a>
             </div>
         </div>
     )
